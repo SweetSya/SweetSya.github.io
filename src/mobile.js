076d88a75200111dd0eff536697ec468e1e85c39
SweetSy <herrysans@yahoo.com>
2023-05-28 20:24:24 +0700

    mobile view

--- src/mobile.js
+++ src/mobile.js
@@ -13,3 +13,18 @@ function detectMob() {
         return navigator.userAgent.match(toMatchItem);
     });
 }
+
+const initMobileView_sidenav = () => {
+    const side = document.querySelector('#side-fixed')
+    side.classList.remove(['justify-between', 'h-screen'])
+    side.classList.add('justify-center')
+
+    document.querySelector('#sidenav-dot').classList.remvoe('flex-grow')
+    document.querySelector('#page-down').children[0].classList.add('-mb-10')
+}
+
+//run if its mobile
+if (!detectMob) {
+    alert('using Mobile view')
+    initMobileView_sidenav()
+}
