60e8ae158f14f6d5e7026d91fe0d3c6356cf8d52
SweetSy <herrysans@yahoo.com>
2023-05-28 20:37:46 +0700

    mobile view

--- src/mobile.js
+++ src/mobile.js
@@ -16,15 +16,19 @@ function detectMob() {
 
 const initMobileView_sidenav = () => {
     const side = document.querySelector('#side-fixed')
-    side.classList.remove(['justify-between', 'h-screen'])
+    side.classList.remove('justify-between')
+    side.classList.remove('h-screen')
     side.classList.add('justify-center')
+    side.classList.add('gap-8')
 
-    document.querySelector('#sidenav-dot').classList.remvoe('flex-grow')
-    document.querySelector('#page-down').children[0].classList.add('-mb-10')
+    document.querySelector('#sidenav-dot').classList.remove('flex-grow')
+    console.log(document.querySelector('#page-down').children[0])
+    document.querySelector('#page-down').children[0].classList.add('-mb-20')
+    document.querySelector('#page-up').children[0].classList.add('mb-4')
 }
 
 //run if its mobile
-if (detectMob()) {
+if (!detectMob()) {
     alert('using Mobile view')
     initMobileView_sidenav()
 }
